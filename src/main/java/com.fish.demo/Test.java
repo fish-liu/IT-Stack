f9ab8d1b38dc8fec172e4eac34585a16198f1da1
liuxw <xianwei.liu@ucarinc.com>
2020-05-20 14:14:27 +0800

    commit

---
+++ src/main/java/com.fish.demo/Test.java
@@ -0,0 +1,14 @@
+package com.fish.demo;
+
+/**
+ * @author liu
+ * @date 2020/5/20
+ * @since 1.0
+ */
+public class Test {
+
+    public static void main(String args[]){
+        System.out.println("hello world!");
+    }
+
+}
